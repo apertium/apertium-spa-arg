1766f8b0a9fe0473ed74df38a72af9fb20132603
Francis M. Tyers <ftyers@prompsit.com>
2010-10-21 09:22:59 +0000

    més
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/incubator/apertium-es-an@26431 72bbbca6-d526-0410-a7d9-f06f51895060

--- dev/testvoc.sh
+++ dev/testvoc.sh
@@ -1,2 +1,2 @@
 #sh inconsistency.sh es-an > /tmp/es-an.testvoc ; sh inconsistency-summary.sh /tmp/es-an.testvoc es-an
-sh inconsistency.sh an-es > /tmp/an-es.testvoc ; sh inconsistency-summary.sh /tmp/an-es.testvoc an-es
+bash inconsistency.sh an-es > /tmp/an-es.testvoc ; bash inconsistency-summary.sh /tmp/an-es.testvoc an-es
