8ff44290f52de1dbba71ba59c6e22f39bb757a4d
Juan Pablo Martínez Cortés <jpmart@unizar.es>
2011-01-09 17:28:13 +0000

    adding new verbs + solving problems with nada, nadie, etc.
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/incubator/apertium-es-an@28250 72bbbca6-d526-0410-a7d9-f06f51895060

--- dev/testvoc.sh
+++ dev/testvoc.sh
@@ -1,2 +1,2 @@
 #sh inconsistency.sh es-an > /tmp/es-an.testvoc ; sh inconsistency-summary.sh /tmp/es-an.testvoc es-an
-bash inconsistency.sh an-es > /tmp/an-es.testvoc ; bash inconsistency-summary.sh /tmp/an-es.testvoc an-es
+bash inconsistency.sh an-es > an-es.testvoc ; bash inconsistency-summary.sh an-es.testvoc an-es
