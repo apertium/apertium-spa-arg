5a70531e8b5d1d62875c164b8040cf68773923e8
Juan Pablo Martínez Cortés <juanpabl@users.sourceforge.net>
2011-02-15 21:07:46 +0000

    change inconsistency.sh (before, it took RL entries instead of LR)
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/incubator/apertium-es-an@28710 72bbbca6-d526-0410-a7d9-f06f51895060

--- dev/inconsistency.sh
+++ dev/inconsistency.sh
@@ -1,8 +1,8 @@
-TMPDIR=/tmp
+TMPDIR=./tmp
 
 if [[ $1 = "an-es" ]]; then
-
-lt-expand ../apertium-es-an.an.dix | grep -v '<prn><enc>' | grep -e ':<:' -e '\w:\w' | sed 's/:<:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
+   
+lt-expand ../apertium-es-an.an.dix | grep -v REGEX | grep -v '<prn><enc>' | sed 's/:>:/%/g' | grep -v ':<:' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
         apertium-pretransfer|
         apertium-transfer ../apertium-es-an.trules-an-es.xml  ../trules-an-es.bin  ../an-es.autobil.bin | tee $TMPDIR/tmp_testvoc2.txt |
         lt-proc -d ../an-es.autogen.bin > $TMPDIR/tmp_testvoc3.txt
@@ -10,7 +10,7 @@ paste -d _ $TMPDIR/tmp_testvoc1.txt $TMPDIR/tmp_testvoc2.txt $TMPDIR/tmp_testvoc
 
 elif [[ $1 = "es-an" ]]; then
 
-lt-expand ../apertium-es-an.es.dix | grep -v '<prn><enc>' | grep -e ':<:' -e '\w:\w' | sed 's/:<:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
+lt-expand ../apertium-es-an.es.dix | grep -v REGEX | grep -v '<prn><enc>' | sed 's/:>:/%/g' | grep -v ':<:' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
         apertium-pretransfer|
         apertium-transfer ../apertium-es-an.trules-es-an.xml ../trules-es-an.bin  ../es-an.autobil.bin | tee $TMPDIR/tmp_testvoc2.txt |
         lt-proc -d ../es-an.autogen.bin > $TMPDIR/tmp_testvoc3.txt
