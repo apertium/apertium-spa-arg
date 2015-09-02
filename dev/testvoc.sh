ae7f041a9f5db35501cafab9f7799fddbda925ca
Juan Pablo Martínez Cortés <jpmart@unizar.es>
2015-09-02 00:16:30 +0000

    copy bidix and t1x from apertium-es-an
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/trunk/apertium-spa-arg@61670 72bbbca6-d526-0410-a7d9-f06f51895060

--- dev/testvoc.sh
+++
@@ -1,2 +0,0 @@
-bash inconsistency.sh es-an > es-an.testvoc ; bash inconsistency-summary.sh es-an.testvoc es-an
-bash inconsistency.sh an-es > an-es.testvoc ; bash inconsistency-summary.sh an-es.testvoc an-es
