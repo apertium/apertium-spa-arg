28fa096bc6c4e7133514d2adbf5397b922134e9f
Juan Pablo Martínez Cortés <jpmart@unizar.es>
2015-09-02 05:51:26 +0000

    testvoc

---
+++ dev/testvoc.sh
@@ -0,0 +1,2 @@
+bash inconsistency.sh spa-arg > spa-arg.testvoc ; bash inconsistency-summary.sh spa-arg.testvoc spa-arg
+bash inconsistency.sh arg-spa > arg-spa.testvoc ; bash inconsistency-summary.sh arg-spa.testvoc arg-spa
