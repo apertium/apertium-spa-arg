aa70645e9513897a0e7d07bf588c946db3cd4733
Juan Pablo Martínez Cortés <jpmart@unizar.es>
2016-01-03 16:27:04 +0000

    some fixes and additions; pa vs ta in arg generation; testvoc spa-arg ok
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/trunk/apertium-spa-arg@64364 72bbbca6-d526-0410-a7d9-f06f51895060

--- dev/inconsistency.sh
+++ dev/inconsistency.sh
@@ -2,17 +2,18 @@ TMPDIR=tmp
 
 if [[ $1 = "arg-spa" ]]; then
    
-lt-expand ../../../languages/apertium-arg/apertium-arg.arg.dix | grep -v REGEX | grep -v '<prn><enc>' | sed 's/:>:/%/g' | grep -v ':<:' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
-        apertium-pretransfer|
-        apertium-transfer ../apertium-spa-arg.arg-spa.t1x  ../arg-spa.t1x.bin  ../arg-spa.autobil.bin | tee $TMPDIR/tmp_testvoc2.txt |
+lt-expand ../../../languages/apertium-arg/apertium-arg.arg.dix | grep -v REGEX | grep -v '<prn><enc>' | sed 's/:>:/%/g' | grep -v ':<:' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |\
+        apertium-pretransfer|\
+	lt-proc -b ../arg-spa.autobil.bin |lrx-proc ../arg-spa.autolex.bin |apertium-transfer -b ../apertium-spa-arg.arg-spa.t1x  ../arg-spa.t1x.bin | tee $TMPDIR/tmp_testvoc2.txt |\
         lt-proc -d ../../../languages/apertium-spa/spa.autogen.bin > $TMPDIR/tmp_testvoc3.txt
 paste -d _ $TMPDIR/tmp_testvoc1.txt $TMPDIR/tmp_testvoc2.txt $TMPDIR/tmp_testvoc3.txt | sed 's/\^.<sent>\$//g' | sed 's/_/   --------->  /g'
+
 
 elif [[ $1 = "spa-arg" ]]; then
 
 lt-expand ../../../languages/apertium-spa/apertium-spa.spa.dix | grep -v REGEX | grep -v '<prn><enc>' | sed 's/:>:/%/g' | grep -v ':<:' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |\
         apertium-pretransfer|\
-        apertium-transfer ../apertium-spa-arg.spa-arg.t1x ../spa-arg.t1x.bin  ../spa-arg.autobil.bin | tee $TMPDIR/tmp_testvoc2.txt |\
+        lt-proc -b ../spa-arg.autobil.bin |lrx-proc ../spa-arg.autolex.bin |apertium-transfer -b ../apertium-spa-arg.spa-arg.t1x  ../spa-arg.t1x.bin | tee $TMPDIR/tmp_testvoc2.txt |\
         lt-proc -d ../../../languages/apertium-arg/arg.autogen.bin > $TMPDIR/tmp_testvoc3.txt
 paste -d _ $TMPDIR/tmp_testvoc1.txt $TMPDIR/tmp_testvoc2.txt $TMPDIR/tmp_testvoc3.txt | sed 's/\^.<sent>\$//g' | sed 's/_/   --------->  /g'
 
