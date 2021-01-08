1aa0d710d5e2f356fa693ab39a0f284cb7874e8f
Juanpabl <jpmart@unizar.es>
2021-01-08 08:31:40 +0000

    fixes to clear testvoc spa-arg

--- dev/inconsistency.sh
+++ dev/inconsistency.sh
@@ -11,7 +11,7 @@ paste -d _ $TMPDIR/tmp_testvoc1.txt $TMPDIR/tmp_testvoc2.txt $TMPDIR/tmp_testvoc
 
 elif [[ $1 = "spa-arg" ]]; then
 
-lt-expand ../../apertium-spa/apertium-spa.spa.dix | grep -v REGEX | grep -v '<prn><enc>' | sed 's/:>:/%/g' | grep -v ':<:' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |\
+lt-expand ../../apertium-spa/apertium-spa.spa.metadix | grep -v REGEX | grep -v '<prn><enc>' | sed 's/:>:/%/g' | grep -v ':<:' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |\
         apertium-pretransfer|\
         lt-proc -b ../spa-arg.autobil.bin |lrx-proc ../spa-arg.autolex.bin |apertium-transfer -b ../apertium-spa-arg.spa-arg.t1x  ../spa-arg.t1x.bin | tee $TMPDIR/tmp_testvoc2.txt |\
         lt-proc -d ../../apertium-arg/arg.autogen.bin > $TMPDIR/tmp_testvoc3.txt
